9ea32b985c7d42899d921e27f5d43f1227d06004
ilosemymind <61050909+ilosemymind@users.noreply.github.com>
2023-10-04 23:57:47 +0200

    Add embla carousel

---
+++ src/js/embla-nav.js
@@ -0,0 +1,51 @@
+export const addDotBtnsAndClickHandlers = (
+  emblaApi,
+  dotsNode,
+	currentSlideCounterNode,
+  onButtonClick
+) => {
+  let dotNodes = [];
+
+  const addDotBtnsWithClickHandlers = () => {
+    dotsNode.innerHTML = emblaApi
+      .scrollSnapList()
+      .map(() => `
+				<button 
+					id="embla-nav__dot" 
+					type="button"
+					class="w-[82px] h-1 bg-gray-500 data-[active=true]:bg-aqua-500 rounded-full transition-colors durarion-50"
+					data-active="false"
+				></button>
+			`)
+      .join('')
+
+    const scrollTo = (index) => {
+      emblaApi.scrollTo(index);
+      if (onButtonClick) onButtonClick(emblaApi);
+    }
+
+    dotNodes = Array.from(dotsNode.querySelectorAll('#embla-nav__dot'))
+    dotNodes.forEach((dotNode, index) => {
+      dotNode.addEventListener('click', () => scrollTo(index), false);
+    })
+  }
+
+  const toggleDotBtnsActive = () => {
+    const previous = emblaApi.previousScrollSnap()
+    const selected = emblaApi.selectedScrollSnap()
+    dotNodes[previous].dataset.active = false;
+    dotNodes[selected].dataset.active = true;
+		if(currentSlideCounterNode) currentSlideCounterNode.innerText = `0${emblaApi.selectedScrollSnap() + 1}`;
+  }
+
+  emblaApi
+    .on('init', addDotBtnsWithClickHandlers)
+    .on('reInit', addDotBtnsWithClickHandlers)
+    .on('init', toggleDotBtnsActive)
+    .on('reInit', toggleDotBtnsActive)
+    .on('select', toggleDotBtnsActive)
+
+  return () => {
+    dotsNode.innerHTML = ''
+  }
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,23 @@
-import '/styles/index.css';
+import EmblaCarousel from 'embla-carousel';
+import Autoplay from 'embla-carousel-autoplay';
+import { addDotBtnsAndClickHandlers } from './embla-nav';
+
+const emblaNode = document.querySelector('#embla');
+const dotsNode = document.querySelector('#embla-nav__dots');
+const currentSlideCounterNode = document.querySelector('#embla-nav__current');
+
+const options = { axis: "x", loop: true }
+
+const emblaApi = EmblaCarousel(emblaNode, options, [Autoplay({ delay: 3000 })]);
+
+const removeDotBtnsAndClickHandlers = addDotBtnsAndClickHandlers(
+  emblaApi, dotsNode, currentSlideCounterNode
+);
+
+emblaApi.on('destroy', removeDotBtnsAndClickHandlers);
+
+
+
 // import viteLogo from '/vite.svg'
 
 // document.querySelector('#app').innerHTML = `
