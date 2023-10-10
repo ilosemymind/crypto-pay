d3f39f777f73990350efdc0ce46a57b9aaa20e8a
ilosemymind <61050909+ilosemymind@users.noreply.github.com>
2023-10-10 02:41:41 +0200

    Change embla imports

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
-import EmblaCarousel from 'embla-carousel';
-import Autoplay from 'embla-carousel-autoplay';
+// import EmblaCarousel from 'embla-carousel';
+// import Autoplay from 'embla-carousel-autoplay';
 import { addDotBtnsAndClickHandlers } from './embla-nav';
 import { listen } from './utils';
 import { enter, leave } from './transition';
